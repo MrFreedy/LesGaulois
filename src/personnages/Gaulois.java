590c8c28ab013812c0536913a9290d1bdd89adef
Arthur <arthur.lenne21@gmail.com>
2022-09-26 18:41:11 +0200

    Update Gaulois.java

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,24 @@
 package personnages;
 
 public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
 
 }
